7faf4d4764f39ea8200b0043b61150fdce33a25b
Pierre Ballif <pierre@ballif.eu>
2025-05-15 19:55:12 +0200

    RVIZ plugin to launch drones

---
+++ src/Utils/rviz_plugins/src/trigger_panel.hpp
@@ -0,0 +1,32 @@
+#ifndef RVIZ_TRIGGER_PANEL_H
+#define RVIZ_TRIGGER_PANEL_H
+
+#include <ros/ros.h>
+#include <rviz/panel.h>
+#include <QLabel>
+#include <QPushButton>
+
+namespace rviz
+{
+class RvizTriggerPanel : public rviz::Panel
+{
+  Q_OBJECT
+public:
+  explicit RvizTriggerPanel(QWidget * parent = 0);
+  ~RvizTriggerPanel() override;
+
+  void onInitialize() override;
+
+protected:
+  ros::NodeHandle nh_;
+  ros::Publisher pub_trigger_;
+
+  QLabel* label_;
+  QPushButton* button_;
+
+private Q_SLOTS:
+  void buttonActivated();
+};
+}  // namespace rviz
+
+#endif //RVIZ_TRIGGER_PANEL_H
